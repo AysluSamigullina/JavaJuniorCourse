fe9260967cb7e19f4532598cc67e6da990292b8b
ayslu samigullina <aislu-bgu@yandex.ru>
2024-04-18 11:51:36 +0300

    Rename Main.java to MainNew.java

--- MainNew.java
+++ MainNew.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Java");
-        System.out.println(args[0]);
+	System.out.println("using git");
     }
 }
